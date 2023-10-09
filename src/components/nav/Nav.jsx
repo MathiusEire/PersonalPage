6945b6a7369bca886edcefb344d176d6a888d698
Matthew Armstrong <36198042+MathiusEire@users.noreply.github.com>
2023-10-09 19:43:42 +0100

    Updated Nav bar element
    -updated the css files for most elements
    - upgraded the nav bar to be cool :)
    - added some react icons and google fonts for future use

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {MdOutlineWavingHand} from 'react-icons/md'
+import {MdWorkOutline} from 'react-icons/md'
+import {GiAutoRepair} from 'react-icons/gi'
+import {AiOutlineProject} from 'react-icons/ai'
+
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome/></a>
+      <a href="#about"><MdOutlineWavingHand/></a>
+      <a href="#experience"><MdWorkOutline/></a>
+      <a href="#services"><GiAutoRepair/></a>
+      <a href="#portfolio"><AiOutlineProject/></a>
+    </nav>
   )
 }
 
